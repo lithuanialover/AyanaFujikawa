c4ba9bc43e15e6ff0360b1b4ab8b81a11f29eceb
lithuanialover <fujikawaayana@gmail.com>
2022-10-01 22:42:07 +0900

    signin.blade.php&signup.blade.phpを表示できた

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,13 @@ Route::get('/home', function () {
     return view('home');
 });
 
+Route::get('/signin', function () {
+    return view('signin');
+});
+Route::get('/signup', function () {
+    return view('signup');
+});
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes

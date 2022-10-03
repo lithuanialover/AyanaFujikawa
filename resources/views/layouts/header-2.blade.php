87b493f14c245459014992d5cb3b56d93e952e92
lithuanialover <fujikawaayana@gmail.com>
2022-10-03 13:12:25 +0900

    header-2.blade.phpにlogout機能実装

--- resources/views/layouts/header-2.blade.php
+++ resources/views/layouts/header-2.blade.php
@@ -1,13 +1,26 @@
 <header class="header flex-item">
+
     <img src="img/logo-green.PNG" alt="estraロゴ" class="header-logo">
     <nav class="header-nav">
       <ul class="header-nav-list flex-item">
         <li><a href="#orderonline" class="header-nav-item">Order Online</a></li>
-        <li><a href="#news"  class="header-nav-item">News</a></li>
-        <li><a href="#about"  class="header-nav-item">About</a></li>
+        <li class="header-nav-item">
+          <div>
+            <div>ログイン中のアカウント</div>
+            <div>{{ Auth::user()->name }}</div>
+        </div>
+        </li>
         <li><a class="header-nav-item">|</a></li>
         <li><a href=""  class="header-nav-item">Account</a></li>
-        <li><a href=""  class="header-nav-item">Logout</a></li>
+        <li>
+          <!-- Authentication -->
+          <form method="POST" action="{{ route('logout') }}">
+          @csrf
+            <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
+            {{ __('Log Out') }}
+            </x-responsive-nav-link>
+          </form>
+        </li>
       </ul>
     </nav>
 </header>
